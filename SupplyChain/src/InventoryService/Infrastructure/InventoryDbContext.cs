9f57596a3562423671f469e689ebfe8c07ae9ff5
ahmadrezahoseinpour <ahmadrezahp13802@gmail.com>
2025-05-11 17:41:15 +0330

    Create InventoryDbContext

---
+++ SupplyChain/src/InventoryService/Infrastructure/InventoryDbContext.cs
@@ -0,0 +1,23 @@
+﻿using InventoryService.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryService.Infrastructure
+{
+    public class InventoryDbContext : DbContext
+    {
+        public DbSet<Inventory> MyProperty { get; set; }
+        public InventoryDbContext(DbContextOptions<InventoryDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Inventory>()
+                .HasKey(i => i.Id);
+
+            modelBuilder.Entity<Inventory>()
+                .Property(i => i.Quantity)
+                .IsConcurrencyToken();
+        }
+    }
+}
