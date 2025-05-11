8c8f79577eecaea71d28e38991f188a6f04daebc
ahmadrezahoseinpour <ahmadrezahp13802@gmail.com>
2025-05-11 17:18:26 +0330

    Create Invertory

---
+++ SupplyChain/src/InventoryService/Domain/Inventory.cs
@@ -0,0 +1,22 @@
+﻿namespace InventoryService.Domain
+{
+    public class Inventory
+    {
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public Guid WarehouseId { get; set; }
+        public int Quantity { get; set; }
+        public Inventory(Guid productId, Guid warehouse, int quantity)
+        {
+            Id = Guid.NewGuid();
+            ProductId = productId;
+            WarehouseId = warehouse;
+            Quantity = quantity;
+        }
+        public void UpdateQuantity(int quantity)
+        {
+            if (quantity < 0) { throw new InvalidOperationException("Quantity cannot be negative."); }
+            Quantity = quantity;
+        }
+    }
+}
