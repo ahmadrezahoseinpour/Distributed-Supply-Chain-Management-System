ab6409167baa531ef70d2ac31448f8923076b33b
ahmadrezahoseinpour <ahmadrezahp13802@gmail.com>
2025-05-12 12:03:39 +0330

    Create InventoryController

---
+++ SupplyChain/src/InventoryService/Controllers/InventoryController.cs
@@ -0,0 +1,25 @@
+﻿using InventoryService.Application;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public InventoryController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateInventory([FromBody] UpdateInventoryCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return result ? Ok() : BadRequest("Failed to update inventory.");
+        }
+    }
+}
